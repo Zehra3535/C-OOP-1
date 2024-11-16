262eb88b5cec179b9135ece18a6d861b8add10ea
Zehra YILDIZ <Zehra YILDIZ@DESKTOP-G1N658G>
2024-11-16 08:01:53 +0300

    Proje dosyası ekle.

---
+++ C# OOP 1/Ogrenci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace C__OOP_1
+{
+    public class Ogrenci
+    {
+        public int age;
+        public string name;
+
+        public int sinif;
+        public int id;
+        public void print()
+        {
+            Console.WriteLine("Name:{0} \t Age:{1}", name,age);
+        }
+
+        public void EnterData(int s,int i)
+        { 
+            sinif=s;
+            id = i;
+            Console.WriteLine("Sınıf:{0} \t Id:{1}",sinif,id);
+        }
+
+        public void Method3(int sinif,int id)
+        {
+            this.sinif = sinif;
+            this.id = id;
+
+            Console.WriteLine("Sinif:{0} \t id:{1}", sinif, id);
+        }
+
+    }
+
+}
---
+++ C# OOP 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__OOP_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci referance1 = new Ogrenci();
+             referance1.name = "Sam Smith";
+            referance1.age = 15;
+           
+            referance1.print();
+            referance1.EnterData(10, 23);
+
+            referance1.Method3(513,4);
+
+            Console.ReadKey();
+        }
+    }
+}
